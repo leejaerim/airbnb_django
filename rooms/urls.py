07fe11b1fb7ef6626d00f584909f1d3daca66ed7
leejaerim <ger6608@naver.com>
2022-10-15 19:26:09 +0900

    Lecture .10 REST API

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import Amenities, AmenityDetail
 
 urlpatterns = [
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
     path("", views.see_all_rooms),
     path("<int:room_pk>", views.see_one_room),
 ]
