5f32fa03325560f59c3782ffa650008e4b116988
leejaerim <ger6608@naver.com>
2022-11-03 22:32:36 +0900

    Lecture .15 Authentication

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,12 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = "uploads"
 
 MEDIA_URL = "user-uploads/"
+
+PAGE_SIZE = 3
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "config.authentication.TrustMeBroAuthentication",
+    ]
+}
