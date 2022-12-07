67b6d06e5240f5b2df522eefbbb32a14f65194ea
leejaerim <ger6608@naver.com>
2022-12-07 22:22:09 +0900

    render.yaml

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,6 @@ else:
     DATABASES = {
         "default": dj_database_url.config(
             # Feel free to alter this value to suit your needs.
-            default="postgresql://postgres:postgres@localhost:5432/mysite",
             conn_max_age=600,
         )
     }
